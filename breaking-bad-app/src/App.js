5bc93641bcb1a10398f8fdf78289f4552acf0a42
Emiliano Diaz <emildsn00@gmail.com>
2022-02-10 15:45:34 -0300

    agrego estilos index.css,agrego img,button,p y span a App.js

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -2,7 +2,17 @@
 
 function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button>Get Another</button>
+      <p>This is a quote
+        <br />
+        <span>- Author </span>
+      </p>
+    </div>
   );
 }
 
